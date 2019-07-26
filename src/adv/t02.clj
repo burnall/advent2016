8ddd13645b6f479037ce03933da4c922c2831c5b
Maxim Fainberg <maksym.fainberg@mobenga.com>
2019-07-26 11:15:28 +0200

    Day 2 - fix & part 2

--- src/adv/t02.clj
+++ src/adv/t02.clj
@@ -26,12 +26,42 @@
 (defn move [pos cmd]
   (map add-lim pos (gradients cmd)))
 
-(defn apply-cmds [cmds]
-  (get-in field 
-          (reduce move [1 1] cmds)))
-          
-
+(defn apply-cmds [pos cmds]
+  (reduce move pos cmds))
+         
 (defn solve
-  ([cmds-list] (map apply-cmds cmds-list))
+  ([cmds-list] 
+    (->> cmds-list 
+         (reductions apply-cmds [1 1])
+         (drop 1)
+         (map (partial get-in field))))
   ([] (solve input)))
+
+; Part 2
+(def transitions {
+  1 {\D 3}
+  2 {\R 3, \D 6}
+  3 {\U 1, \L 2, \R 4, \D 7}
+  4 {\L 3, \D 8}
+  5 {\R 6}
+  6 {\U 2, \L 5, \R 7, \D 10}
+  7 {\L 6, \U 3, \R 8, \D 11}
+  8 {\L 7, \U 4, \R 9, \D 12}
+  9 {\L 8}
+  10 {\U 6, \R 11}
+  11 {\L 10, \U 7, \R 12, \D 13}
+  12 {\L 11, \U 8}
+  13 {\U 11}})
+
+(defn make-moves [pos cmds]
+  (reduce (fn [pos cmd] (get-in transitions [pos cmd] pos))
+          pos 
+          cmds))
+
+(defn solve2 
+  ([cmds-list] 
+    (drop 1 
+          (reductions make-moves 5 cmds-list)))
+  ([] (solve2 input)))
+
 
