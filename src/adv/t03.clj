ae7b82e3e9301213311639cb3c619ededc042aa5
Maxim Fainberg <maksym.fainberg@mobenga.com>
2019-07-30 00:39:43 +0200

    Day 3

---
+++ src/adv/t03.clj
@@ -0,0 +1,39 @@
+(ns adv.t03
+  (:require [adv.util :refer [split split-lines parse-int]]))
+
+(defn parse-cmd [cmd] 
+  {:turn (keyword (subs cmd 0 1))
+   :distance (parse-int (subs cmd 1))})
+
+(defn get-sides [line]
+  (->> (split line #" +")
+       (drop 1)
+       (map parse-int)))
+
+(def input 
+  (->> "data/t03.txt"
+       (slurp)
+       (split-lines)
+       (map get-sides)))
+
+(defn valid? [sides]
+  (let [[a b c] (sort sides)]
+    (< c (+ a b))))
+
+(defn solve 
+  ([triangles] 
+    (->> triangles
+         (filter valid?)
+         (count)))
+  ([] (solve input)))
+
+; Part 2
+(defn rows-to-cols [rows]
+  (->> (concat (map first rows) 
+               (map second rows)
+               (map #(nth % 2) rows))
+       (partition 3))) 
+
+(defn solve2 
+  ([rows] (solve (rows-to-cols rows)))
+  ([] (solve2 input)))
